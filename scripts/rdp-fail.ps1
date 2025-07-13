b6ae0c83659d7b006319e0f19e9a0ca427b61303
gnegn <gnegenngne@gmail.com>
2025-07-13 13:29:51 +0300

    ui for new commands

--- scripts/rdp-fail.ps1
+++
@@ -1,9 +0,0 @@
-# Показує невдалі входи по RDP за останні 7 днів
-Get-WinEvent -FilterHashtable @{
-    LogName='Security';
-    ID=4625;
-    StartTime=(Get-Date).AddDays(-7)
-} | Where-Object {
-    $_.Properties[18].Value -eq "10"  # Logon Type 10 = RDP
-} | Select-Object TimeCreated, @{Name="Account";Expression={$_.Properties[5].Value}}, Message |
-Format-Table -AutoSize
